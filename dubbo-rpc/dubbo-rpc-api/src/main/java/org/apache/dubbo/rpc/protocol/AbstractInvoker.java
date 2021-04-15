639277aa5c2c80ffcf5145a5a5e43426fa3f942b
dan.wang1030 <18730601431@163.com>
2021-04-15 18:57:20 +0800

    上传rpc相关源码

---
+++ dubbo-rpc/dubbo-rpc-api/src/main/java/org/apache/dubbo/rpc/protocol/AbstractInvoker.java
@@ -0,0 +1,192 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.dubbo.rpc.protocol;
+
+import org.apache.dubbo.common.Constants;
+import org.apache.dubbo.common.URL;
+import org.apache.dubbo.common.Version;
+import org.apache.dubbo.common.logger.Logger;
+import org.apache.dubbo.common.logger.LoggerFactory;
+import org.apache.dubbo.common.utils.NetUtils;
+import org.apache.dubbo.rpc.Invocation;
+import org.apache.dubbo.rpc.Invoker;
+import org.apache.dubbo.rpc.Result;
+import org.apache.dubbo.rpc.RpcContext;
+import org.apache.dubbo.rpc.RpcException;
+import org.apache.dubbo.rpc.RpcInvocation;
+import org.apache.dubbo.rpc.RpcResult;
+import org.apache.dubbo.rpc.support.RpcUtils;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * AbstractInvoker.
+ */
+public abstract class AbstractInvoker<T> implements Invoker<T> {
+
+    protected final Logger logger = LoggerFactory.getLogger(getClass());
+
+    private final Class<T> type;
+
+    private final URL url;
+
+    /**
+     * 可以保存属性，也可以是一些接口参数信息比如token
+     */
+    private final Map<String, String> attachment;
+
+    private volatile boolean available = true;
+
+    private AtomicBoolean destroyed = new AtomicBoolean(false);
+
+    public AbstractInvoker(Class<T> type, URL url) {
+        this(type, url, (Map<String, String>) null);
+    }
+
+    public AbstractInvoker(Class<T> type, URL url, String[] keys) {
+        this(type, url, convertAttachment(url, keys));
+    }
+
+    public AbstractInvoker(Class<T> type, URL url, Map<String, String> attachment) {
+        if (type == null) {
+            throw new IllegalArgumentException("service type == null");
+        }
+        if (url == null) {
+            throw new IllegalArgumentException("service url == null");
+        }
+        this.type = type;
+        this.url = url;
+        this.attachment = attachment == null ? null : Collections.unmodifiableMap(attachment);
+    }
+
+    private static Map<String, String> convertAttachment(URL url, String[] keys) {
+        if (keys == null || keys.length == 0) {
+            return null;
+        }
+        Map<String, String> attachment = new HashMap<String, String>();
+        for (String key : keys) {
+            String value = url.getParameter(key);
+            if (value != null && value.length() > 0) {
+                attachment.put(key, value);
+            }
+        }
+        return attachment;
+    }
+
+    @Override
+    public Class<T> getInterface() {
+        return type;
+    }
+
+    @Override
+    public URL getUrl() {
+        return url;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return available;
+    }
+
+    protected void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    @Override
+    public void destroy() {
+        if (!destroyed.compareAndSet(false, true)) {
+            return;
+        }
+        setAvailable(false);
+    }
+
+    public boolean isDestroyed() {
+        return destroyed.get();
+    }
+
+    @Override
+    public String toString() {
+        return getInterface() + " -> " + (getUrl() == null ? "" : getUrl().toString());
+    }
+
+    @Override
+    public Result invoke(Invocation inv) throws RpcException {
+        // if invoker is destroyed due to address refresh from registry, let's allow the current invoke to proceed
+        //当注册中心刷新地址时销毁该Invoker，依然让当前的Invoke方法执行
+        //TODO ,何时被设置成destroyed=true
+        if (destroyed.get()) {
+            logger.warn("Invoker for service " + this + " on consumer " + NetUtils.getLocalHost() + " is destroyed, "
+                    + ", dubbo version is " + Version.getVersion() + ", this invoker should not be used any longer");
+        }
+        RpcInvocation invocation = (RpcInvocation) inv;
+        invocation.setInvoker(this);
+        //将当前的attachment 写入invocation
+        if (attachment != null && attachment.size() > 0) {
+            invocation.addAttachmentsIfAbsent(attachment);
+        }
+        //获取context中的attachments并写入
+        Map<String, String> contextAttachments = RpcContext.getContext().getAttachments();
+        if (contextAttachments != null && contextAttachments.size() != 0) {
+            /**
+             * invocation.addAttachmentsIfAbsent(context){@link RpcInvocation#addAttachmentsIfAbsent(Map)}should not be used here,
+             * because the {@link RpcContext#setAttachment(String, String)} is passed in the Filter when the call is triggered
+             * by the built-in retry mechanism of the Dubbo. The attachment to update RpcContext will no longer work, which is
+             * a mistake in most cases (for example, through Filter to RpcContext output traceId and spanId and other information).
+             */
+            //TODO 和重试的filter有关，需要看一下为什么
+            invocation.addAttachments(contextAttachments);
+        }
+        //判断当前接口是否走异步
+        if (getUrl().getMethodParameter(invocation.getMethodName(), Constants.ASYNC_KEY, false)) {
+            invocation.setAttachment(Constants.ASYNC_KEY, Boolean.TRUE.toString());
+        }
+        //如果走异步，则在attachment中添加invocationId属性
+        RpcUtils.attachInvocationIdIfAsync(getUrl(), invocation);
+
+
+        try {
+            //执行调用流程， 这里运用哪个协议，走那个协议的invoke流程
+            return doInvoke(invocation);
+        } catch (InvocationTargetException e) { // biz exception
+            //如果发生异常，将异常封装到result中
+            Throwable te = e.getTargetException();
+            if (te == null) {
+                return new RpcResult(e);
+            } else {
+                if (te instanceof RpcException) {
+                    ((RpcException) te).setCode(RpcException.BIZ_EXCEPTION);
+                }
+                return new RpcResult(te);
+            }
+        } catch (RpcException e) {
+            if (e.isBiz()) {
+                return new RpcResult(e);
+            } else {
+                throw e;
+            }
+        } catch (Throwable e) {
+            return new RpcResult(e);
+        }
+    }
+
+    protected abstract Result doInvoke(Invocation invocation) throws Throwable;
+
+}
